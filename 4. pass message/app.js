952e36546eb6aa7923c049abdffde8d25a7cb0b2
DohnalMichal <michal.dohnal@atlas.cz>
2020-09-13 18:22:53 +0200

    4th project done

---
+++ 4. pass message/app.js
@@ -0,0 +1,20 @@
+let input = document.querySelector('input');
+const button = document.querySelector('button');
+let answer = document.querySelector('.message');
+
+button.addEventListener("click", submitFunction);
+function submitFunction() {
+  if (input.value.length === 0) {
+    setTimeout(hideWarningMessage, 2000);
+    document.querySelector('.warning-message').style.display = "block";
+    console.log("wrong");
+  } else {
+    answer.innerHTML = input.value;
+    input.value = "";
+  }
+  // answer.innerHTML = "";
+}
+
+function hideWarningMessage() {
+  document.querySelector('.warning-message').style.display = "none";
+}
